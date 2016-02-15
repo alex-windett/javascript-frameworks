0e93b9613a3f58e4f4c2cf45b72d5a05da76a644
Alex Windett <alex@bigfish.co.uk>
2016-02-15 10:10:34 +0000

    initial react setup

---
+++ react-app/src/js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import Archives from "./pages/Archives";
+// import Featured from "./pages/Featured";
+// import Layout from "./pages/Layout";
+// import Settings from "./pages/Settings";
+ console.log("HEllo")
