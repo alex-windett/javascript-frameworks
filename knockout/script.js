1c1a94e1eb27ed2468fd4e91993f1e775fd53746
Alex Windett <windettalex@gmail.com>
2016-02-15 18:06:06 +0000

    start of knockout

--- knockout/script.js
+++ knockout/script.js
@@ -1,11 +1,46 @@
 $(document).ready(function(){
+    /*
+    var teamObserverableArray = ko.observableArray([
+        {id: 1, name:"Alex", sport: "football"},
+        {id: 2, name:"John", sport: "cricket"},
+        {id: 3, name:"Pete", sport: "handball"},
+        {id: 4, name:"Bob", sport: "cycling"},
+        {id: 5, name:"Jim", sport: "rugby"}
+    ]);
 
-    var team = [
-      {id: 1, name:"Alex", sport: "football"},
-      {id: 2, name:"John", sport: "cricket"},
-      {id: 3, name:"Pete", sport: "handball"},
-      {id: 4, name:"Bob", sport: "cycling"},
-      {id: 5, name:"Jim", sport: "rugby"},
-    ];
+    var teamMemberModel = {
+        name: ko.observable("Alex"),
+        sport: ko.observable("Rugby")
+    };
 
+    ko.applyBindings({
+        team: [
+            {id: 1, name:"Alex", sport: "football"},
+            {id: 2, name:"John", sport: "cricket"},
+            {id: 3, name:"Pete", sport: "handball"},
+            {id: 4, name:"Bob", sport: "cycling"},
+            {id: 5, name:"Jim", sport: "rugby"}
+        ]
+    });
+    */
+    
+    var teamView = function(teamMembers){
+        var self = this;
+
+        self.members = ko.observableArray([
+            {id: 1, name:"Alex", sport: "football"},
+            {id: 2, name:"John", sport: "cricket"},
+            {id: 3, name:"Pete", sport: "handball"},
+            {id: 4, name:"Bob", sport: "cycling"},
+            {id: 5, name:"Jim", sport: "rugby"}
+        ])
+    }
+
+    // A single team member
+    var teamMember = function(name, sport) {
+        this.name = ko.observable(name);
+        this.sport = ko.observable(sport)
+    }
+
+    return teamMember;
 });
