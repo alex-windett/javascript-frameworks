909155ca1d9d51c458da073f03b1218138dd8ca1
Alex Windett <alex@bigfish.co.uk>
2016-02-16 12:26:49 +0000

    mapping over returned data from API for team-cat

--- react-app/src/js/components/TeamDropdown.js
+++ react-app/src/js/components/TeamDropdown.js
@@ -1,6 +1,31 @@
 import React from "react";
 import $ from 'jquery';
 
+var itemWrapper = React.createClass({
+
+    render(){
+        return (
+            <option>{this.props.data.id}</option>
+        )
+    }
+})
+
+var OptionList = React.createClass({
+
+    render() {
+
+        var listItem = this.props.data;
+
+        return (
+            <select>
+                {listItem.map(function(item){
+                    return <option value={item.slug} key={item.id}>{item.name}</option>
+                })}
+            </select>
+        )
+    }
+});
+
 export default class TeamDropdown extends React.Component {
 
     constructor(){
@@ -12,32 +37,27 @@ export default class TeamDropdown extends React.Component {
 
         $.ajax({
             url: "http://9bar.alex.dev/wp-json/wp/v2/bf-team-cat",
-            dataType: "JSON",
-            cahce: false,
-            success: function(data){
-                console.log('success! ')
-                console.log(data)
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
             }.bind(this),
-            error: function(xhr, status, err){
-                console.error('error: ', xhr, status, err.toString());
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
             }.bind(this)
 
         })
     }
 
     componentDidMount() {
+        // run function above
         this.loadCatagoryFromServer();
     }
 
     render() {
 
         return (
-            <select>
-                <option value="cycling">Cycling</option>
-                <option value="running">Running</option>
-                <option value="climbing">Climbing</option>
-                <option value="team-gb">Team GB</option>
-            </select>
+                <OptionList data={this.state.data}/>
         )
     }
 }
